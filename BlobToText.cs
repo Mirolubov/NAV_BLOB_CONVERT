0fafcca364e8431c7d31f52fd6a237bfa6ab3cbc
Mirolubov <32896779+Mirolubov@users.noreply.github.com>
2020-02-23 10:01:56 +0300

    Add files via upload

--- BlobToText.cs
+++ BlobToText.cs
@@ -8,23 +8,49 @@ using System.Text;
 public class BlobToText
 {
     [SqlFunction]
-    static public SqlString BlobToNVarChar(SqlBytes sqlBytes)
+    public static SqlString BlobToNVarChar(SqlBytes sqlBytes)
     {
-        SqlString str;
-        byte[] bytes = new byte[1000];
-        byte[] bytesOffset = new byte[1000];
-        Stream bStream = sqlBytes.Stream;
-        int n = bStream.Read(bytes, 0, 1000);
-        Buffer.BlockCopy(bytes, 4, bytesOffset, 0, bytes.Length-4);
-        MemoryStream inStream = new MemoryStream(bytesOffset) ;
-        
-        DeflateStream decompressionStream = new DeflateStream(inStream, CompressionMode.Decompress);
-        
-        MemoryStream outStream = new MemoryStream();
-        decompressionStream.CopyTo(outStream);
+        SqlString str = "";
+        if (sqlBytes.IsNull)
+            return null;
+        try
+        {
 
-        Encoding Rus = Encoding.GetEncoding("windows-1251");
-        str = Rus.GetString(outStream.ToArray());
+            Stream bStream = sqlBytes.Stream;
+            byte[] bytes = new byte[bStream.Length];
+
+            Encoding Rus = Encoding.GetEncoding("windows-1251");
+            int n = bStream.Read(bytes, 0, (int)bStream.Length);
+            if (n > 4)
+            {
+                byte[] MAGIC_NUMBER = new byte[] { 0x02, 0x45, 0x7D, 0x5B };
+                if (bytes[0] == MAGIC_NUMBER[0] && bytes[1] == MAGIC_NUMBER[1] && bytes[2] == MAGIC_NUMBER[2] && bytes[3] == MAGIC_NUMBER[3])
+                {
+                    byte[] bytesOffset = new byte[bytes.Length - 4];
+                    Buffer.BlockCopy(bytes, 4, bytesOffset, 0, bytes.Length - 4);
+                    MemoryStream inStream = new MemoryStream(bytesOffset);
+
+                    DeflateStream decompressionStream = new DeflateStream(inStream, CompressionMode.Decompress);
+
+                    MemoryStream outStream = new MemoryStream();
+                    decompressionStream.CopyTo(outStream);
+
+                    str = Rus.GetString(outStream.ToArray());
+                }
+                else
+                {
+                    str = Rus.GetString(bytes, 0, n);
+                }
+            }
+            else
+            {
+                str = Rus.GetString(bytes, 0, n);
+            }
+        }
+        catch
+        {
+            str = null;
+        }
         return str;
     }
 }
